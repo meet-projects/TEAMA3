725cf59a59515fc43cf110a050d2ac15c2755f2c
Kiosk <kiosk@cs.huji.ac.il>
2015-08-09 14:21:12 +0300

    All databases, works with web_app

--- database_setup.py
+++ database_setup.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String
+from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Person(Base):
-	__table__='users'
+class User(Base):
+	__tablename__='users'
 	id=Column(Integer, primary_key=True)
 	first_name= Column(String)
 	last_name= Column(String)
@@ -15,14 +15,14 @@ class Person(Base):
 	city= Column(String)
 	dob=Column(Date)
 	description=Column(String)
-	cultual_heritage=Column(String)
+	cultural_heritage=Column(String)
 	email=Column(String)
 	status=Column(Boolean)
 	username=Column(String)
 	password=Column(String)
 
 class Example(Base):
-	__table__='examples'
+	__tablename__='examples'
 	id=Column(Integer, primary_key=True)
 	title=Column(String)
 	user_id=Column(Integer)
@@ -30,7 +30,7 @@ class Example(Base):
 	image=Column(String)
 
 class Pair(Base):
-	__table__='pairs'
+	__tablename__='pairs'
 	id=Column(Integer, primary_key=True)
 	id_1=Column(Integer)
 	id_2=Column(Integer)
