83e09300e1e8bbfa9c2900f4a05360ff40c5843c
Kiosk <kiosk@cs.huji.ac.il>
2015-08-09 15:27:41 +0300

    First day, not complete

--- web_app.py
+++ web_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, flash
 app = Flask(__name__)
 
 # SQLAlchemy stuff
@@ -6,7 +6,7 @@ from database_setup import Base, User, Example, Pair
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 engine = create_engine('sqlite:///crudlab.db')
-Base.metadata.bind = engine
+Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
@@ -17,7 +17,7 @@ def main():
 	return render_template('main_page.html')
 
 @app.route('/sign_up', methods = ['GET', 'POST'])
-def sign_in():
+def sign_up():
 	if request.method == 'GET':
 		return render_template('PLACEHOLDER_SIGN_UP.HTML')
 	else:
@@ -32,15 +32,35 @@ def sign_in():
 			cultural_heritage = request.form['cultural_heritage'],
 			email = request.form['email'],
 			status = request.form['status'],
-			user_name = request.form['user_name'],
+			username = request.form['username'],
 			password = request.form['password'],
 			)
 		session.add(new_user)
 		session.commit()
 		return redirect(url_for('main'))
 
+@app.route('/sign_in', methods = ['Get','POST'])
+def sign_in():
+	if request.method == 'GET':
+		return render_template("PLACEHOLDER_SIGN_IN.HTML")
+	else:
+		user = session.query(User).filter_by(username = request.form['username']).first()
+		if user.username == None or user.password != request.form['password']:
+			flash ("Invalid username or password")
+		else:
+			session['username'] = request.form['username']
 
-#@app.route('/sign_in')
+
+
+
+
+
+
+@app.route('/examples')
+def examples():
+	pairs = session.query(Pair).all()
+	return render_template('PLACEHOLDER_EXAMPLES.HTML', pairs)
+
 
 
 
